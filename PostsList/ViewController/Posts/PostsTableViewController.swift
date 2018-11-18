2ab4a217240a651f48e761cb177545f0633216f9
Kostiantyn Bilyk <kos.bilyk@gmail.com>
2018-11-18 20:06:24 +0200

    Started implementation of Posts API.

--- PostsList/ViewController/Posts/PostsTableViewController.swift
+++ PostsList/ViewController/Posts/PostsTableViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class PostsTableViewController: UITableViewController {
+
+    // MARK: - Variables
+
+    var userIdentifier: Int?
+    var posts: Array<Dictionary<String, Any>>? = []
 
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let userIdentifier = userIdentifier {
+            // Reqest
+            Alamofire.request("https://jsonplaceholder.typicode.com/posts?userId=\(String(userIdentifier))").responseJSON(completionHandler: { (response) in
+                print("Request: \(String(describing: response.request))")   // original url request
+                print("Response: \(String(describing: response.response))") // http url response
+                print("Result: \(response.result)")                         // response serialization result
+
+                if let json = response.result.value {
+                    print("JSON: \(json)") // serialized json response
+                }
+
+                if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                    print("Data: \(utf8Text)") // original server data as UTF8 string
+                }
+            })
+        }
     }
 
     // MARK: - Configuration
@@ -24,7 +47,7 @@ class PostsTableViewController: UITableViewController {
             return
         }
 
-        print("\(cell)")
+        print(cell)
     }
 
     // MARK: - Table view data source
@@ -34,7 +57,11 @@ class PostsTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        guard let posts = posts else {
+            return 0
+        }
+
+        return posts.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,50 +71,4 @@ class PostsTableViewController: UITableViewController {
 
         return cell
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
